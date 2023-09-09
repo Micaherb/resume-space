396a49864776d0f60f3217c221b03d651dd22ad3
Micaherb <merb1000@gmail.com>
2023-09-09 13:16:35 -0400

    marathoner and site demo added

--- src/App.js
+++ src/App.js
@@ -3,30 +3,42 @@ import logo from './logo.svg';
 import './App.css';
 import ColoredBlock from './Components/ColoredBlock';
 import HTMLRenderer from './Components/HTMLRenderer';
-import raw from './assets/marathoner.txt';
+import marathoner from './assets/marathoner.txt';
+import honks from './assets/honks.txt';
+import site from './assets/site.txt';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = { data: [] };
+    this.state = { 
+      marathoner: '',
+      honks: '',
+      site: '',
+    };
   }
 
   async componentDidMount() {
-    await fetch(raw)
+    await fetch(marathoner)
       .then(res => res.text())
-      .then(text => this.setState({ data: text }));
+      .then(text => this.setState({ marathoner: text }));
+
+    await fetch(honks)
+      .then(res => res.text())
+      .then(text => this.setState({ honks: text }));
+
+    await fetch(site)
+      .then(res => res.text())
+      .then(text => this.setState({ site: text }));
   }
 
   render() {
-    let htmlString = "<iframe src=\"https://www.lexaloffle.com/bbs/widget.php?pid=honks_of_the_southern_isles\" allowfullscreen width=\"621\" height=\"513\" style=\"border:none; overflow:hidden\"></iframe>";
-    console.log(this.state.data)
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <ColoredBlock text="Hello, World!" color="#FF5733BB" height={850} contentComponent={<HTMLRenderer htmlString={this.state.data} />} />
-          <ColoredBlock text="Micah" color="#5733FFBB" height={750} />
-          <ColoredBlock text="Erb" color="#57FF33BB" height={750} />
+          <ColoredBlock text="Micah" color="#5733FFBB" height={750} contentComponent={<HTMLRenderer htmlString={this.state.honks} />} />
+          <ColoredBlock text="Hello, World!" color="#FF5733BB" height={850} contentComponent={<HTMLRenderer htmlString={this.state.marathoner} />} />
+          <ColoredBlock text="Erb" color="#57FF33BB" height={750} contentComponent={<HTMLRenderer htmlString={this.state.site} />} />
         </header>
       </div>
     );
